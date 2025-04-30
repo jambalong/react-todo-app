4e7a30ca2d4c420f8289ed993128da83b743a94b
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-04-29 21:55:51 -0700

    Add Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input ({ value, onChange, placeholder }) {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+}

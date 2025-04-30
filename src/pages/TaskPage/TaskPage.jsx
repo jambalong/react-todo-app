faa008ae427aac55469665a33cb18b7c2cf6aebb
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-04-29 22:51:19 -0700

    Add handleAddTask() to TaskPage

--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import './TaskPage.css';
 
 function TaskPage() {
     // TODO: Render task page components
+    const [tasks, setTasks] = useState([]);
+
+    const handleAddTask = (newTaskName) => {
+      const newTask = {
+        id: Date.now(),
+        name: newTaskName,
+        completed: false,
+      };
+      setTasks([...tasks, newTask]);
+    };
 
     return (
         <div>

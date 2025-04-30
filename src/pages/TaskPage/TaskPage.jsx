a088505608772804982f805b178e90f451f00dba
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-04-29 21:50:35 -0700

    Add TaskPage header

--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -2,6 +2,12 @@ import './TaskPage.css';
 
 function TaskPage() {
     // TODO: Render task page components
+
+    return (
+        <>
+          <h1 className="header">Task List</h1>
+        </>
+    )
 }
 
 export { TaskPage };

0268a339784bd7bcde425cfd2ebc4300fbac6286
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-04-29 22:44:40 -0700

    Change fragment to div

--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -4,10 +4,10 @@ function TaskPage() {
     // TODO: Render task page components
 
     return (
-        <>
+        <div>
           <h1 className="header">Task List</h1>
-        </>
-    )
+        </div>
+    );
 }
 
 export { TaskPage };
